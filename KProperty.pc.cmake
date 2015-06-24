98992a2f92162a76b8187f2bac12bc87a794852d
Jaroslaw Staniek <staniek@kde.org>
2015-06-24 21:23:38 +0200

    Rename kproperty.pc to KProperty.pc per convention
    +update URL and deps

--- KProperty.pc.cmake
+++ KProperty.pc.cmake
@@ -6,7 +6,7 @@ includedir=${prefix}/include
 Name: KProperty
 Description: A property editing framework with editor
 Version: @KPROPERTY_VERSION@
-URL: https://projects.kde.org/kproperty
-Requires: QtCore QtGui KF5::WidgetsAddons KF5::GuiAddons
+URL: https://community.kde.org/KProperty
+Requires: Qt5Core Qt5Gui KF5WidgetsAddons KF5GuiAddons
 Libs: -L${libdir}
 Cflags: -I${includedir}
