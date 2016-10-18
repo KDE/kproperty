2e82810456dab6af900f3eb22f4a1cbd5571c932
Jaroslaw Staniek <staniek@kde.org>
2016-10-18 21:56:45 +0200

    Fix painting grid over scroll areas

--- src/KPropertyWidgetsFactory.cpp
+++ src/KPropertyWidgetsFactory.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2008-2015 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2008-2016 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -167,7 +167,14 @@ void KPropertyWidgetsFactory::paintTopGridLine(QWidget *widget)
     }
     const QColor gridLineColor(view ? view->gridLineColor() : KPropertyEditorView::defaultGridLineColor());
     if (gridLineColor.isValid()) {
-        QPainter p(widget);
+        const QAbstractScrollArea *area = qobject_cast<const QAbstractScrollArea*>(widget);
+        QWidget *widgetToPaint;
+        if (area && area->viewport()) {
+            widgetToPaint = area->viewport();
+        } else {
+            widgetToPaint = widget;
+        }
+        QPainter p(widgetToPaint);
         p.setPen(QPen( QBrush(gridLineColor), 1));
         p.drawLine(0, 0, widget->width()-1, 0);
     }
