cc480951a2d232e24e02f4283133c5fb7fb7a499
Jaroslaw Staniek <staniek@kde.org>
2017-01-24 23:49:58 +0100

    Add basic autotest for the KProperty class
    TRIVIAL

---
+++ autotests/KPropertyTest.cpp
@@ -0,0 +1,273 @@
+/* This file is part of the KDE project
+   Copyright (C) 2017 Jarosław Staniek <staniek@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
+#include <QtTest>
+#include <KProperty>
+
+class KPropertyTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testNull();
+private:
+};
+
+void KPropertyTest::testNull()
+{
+    {
+        // isNull
+        const KProperty nullProperty;
+        QVERIFY(nullProperty.isNull());
+        const KProperty nullPropertyCopy(nullProperty);
+        QVERIFY(nullPropertyCopy.isNull());
+        QVERIFY2(nullProperty == nullPropertyCopy, "two null properties are equal");
+        QVERIFY2(nullProperty == KProperty(), "two null properties are equal v2");
+        QVERIFY2(!(nullProperty != nullPropertyCopy), "two null properties are equal v2");
+    }
+    {
+        // name
+        const KProperty nullProperty;
+        const KProperty nullPropertyCopy(nullProperty);
+        KProperty notNullProperty("notNull");
+        QVERIFY(!notNullProperty.isNull());
+        QVERIFY2(!(nullProperty == notNullProperty), "null property is not equal to not null property");
+        notNullProperty.setName(QByteArray());
+        QVERIFY2(notNullProperty.isNull(), "Not null property becomes null after setting empty name");
+        notNullProperty.setName("");
+        QVERIFY2(notNullProperty.isNull(), "Not null property becomes null after setting empty name v2");
+        notNullProperty.setName("other");
+        QVERIFY2(!notNullProperty.isNull(), "Null property becomes not-null after setting non-empty name");
+        QVERIFY(nullProperty.name().isEmpty());
+        QVERIFY(nullPropertyCopy.name().isEmpty());
+    }
+    {
+        // caption
+        const KProperty nullProperty;
+        const KProperty nullPropertyCopy(nullProperty);
+        KProperty notNullProperty("notNull");
+        QVERIFY(nullProperty.caption().isEmpty());
+        QVERIFY(nullPropertyCopy.caption().isEmpty());
+        QVERIFY(notNullProperty.caption().isEmpty());
+        KProperty propertyWithCaption("withCaption", 17, "My caption");
+        QVERIFY(!propertyWithCaption.isNull());
+        propertyWithCaption.setName(QByteArray());
+        QVERIFY2(propertyWithCaption.isNull(), "Property with name and caption is null after setting "
+                                               "empty name");
+    }
+    {
+        // caption does not influence null
+        KProperty propertyWithCaption("withCaption", 17, "My caption");
+        propertyWithCaption.setCaption(QByteArray());
+        QVERIFY2(!propertyWithCaption.isNull(), "Property with name and caption is still not null "
+                                                "after setting empty caption");
+    }
+    {
+        // description
+        KProperty nullProperty;
+        nullProperty.setDescription("My desc");
+        QVERIFY2(nullProperty.isNull(), "Null property with non-empty description is still null");
+    }
+    {
+        // value
+        KProperty nullProperty;
+        QTest::ignoreMessage(QtWarningMsg, "COULD NOT SET value to a null property");
+        nullProperty.setValue(0.123);
+        QTest::ignoreMessage(QtWarningMsg, "COULD NOT SET value to a null property");
+        KProperty nullPropertyWithValue(QByteArray(), 177);
+        QVERIFY2(nullPropertyWithValue.isNull(), "Null property with value set from ctor is still null");
+        QVERIFY2(nullPropertyWithValue.value().isNull(), "Null property with value set from "
+                                                                "ctor still has no value");
+    }
+}
+
+/* TODO
+    KProperty::KProperty(KProperty const&)
+    KProperty::KProperty(QByteArray const&, KPropertyListData*, QVariant const&, QString const&, QString const&, int, KProperty*)
+    KProperty::KProperty(QByteArray const&, QStringList const&, QStringList const&, QVariant const&, QString const&, QString const&, int, KProperty*)
+    KProperty::KProperty(QByteArray const&, QVariant const&, QString const&, QString const&, int, KProperty*)
+    KProperty::KProperty()
+    KProperty::captionForDisplaying() const
+    KProperty::caption() const
+    KProperty::childValueChanged(KProperty*, QVariant const&, bool)
+    KProperty::children() const
+    KProperty::child(QByteArray const&)
+    KProperty::clearModifiedFlag()
+    KProperty::composedProperty() const
+    KProperty::description() const
+    KProperty::hasOptions() const
+    KProperty::iconName() const
+    KProperty::isModified() const
+OK  KProperty::isNull() const
+    KProperty::isReadOnly() const
+    KProperty::isStorable() const
+    KProperty::isVisible() const
+    KProperty::listData() const
+    KProperty::name() const
+    KProperty::oldValue() const
+    KProperty::operator=(KProperty const&)
+    KProperty::operator=(QVariant const&)
+    KProperty::operator==(KProperty const&) const
+    KProperty::option(char const*, QVariant const&) const
+    KProperty::parent() const
+    KProperty::resetValue()
+    KProperty::setCaption(QString const&)
+    KProperty::setComposedProperty(KComposedPropertyInterface*)
+    KProperty::setDescription(QString const&)
+    KProperty::setIconName(QString const&)
+    KProperty::setListData(KPropertyListData*)
+    KProperty::setListData(QStringList const&, QStringList const&)
+    KProperty::setName(QByteArray const&)
+    KProperty::setOption(char const*, QVariant const&)
+    KProperty::setReadOnly(bool)
+    KProperty::setStorable(bool)
+    KProperty::setType(int)
+    KProperty::setValueSyncPolicy(KProperty::ValueSyncPolicy)
+    KProperty::setValue(QVariant const&, bool*, QFlags<KProperty::ValueOption>)
+    KProperty::setValue(QVariant const&, bool, bool)
+    KProperty::setVisible(bool)
+    KProperty::type() const
+    KProperty::valueEqualsTo(QVariant const&, QFlags<KProperty::ValueOption>) const
+    KProperty::valueSyncPolicy() const
+    KProperty::value() const
+
+    KPropertyListData::~KPropertyListData()
+    KPropertyListData::KPropertyListData(QList<QVariant>, QStringList const&)
+    KPropertyListData::KPropertyListData(QStringList const&, QStringList const&)
+    KPropertyListData::KPropertyListData()
+    KPropertyListData::keysAsStringList() const
+    KPropertyListData::setKeysAsStringList(QStringList const&)
+*/
+
+/* Full API TODO:
+
+KComposedPropertyCreatorInterface::~KComposedPropertyCreatorInterface()
+KComposedPropertyCreatorInterface::KComposedPropertyCreatorInterface()
+KComposedPropertyInterface::~KComposedPropertyInterface()
+KComposedPropertyInterface::KComposedPropertyInterface(KProperty*)
+KPropertyBuffer::KPropertyBuffer(KPropertySet const&)
+KPropertyBuffer::KPropertyBuffer()
+KPropertyBuffer::init(KPropertySet const&)
+KPropertyBuffer::intersectedChanged(KPropertySet&, KProperty&)
+KPropertyBuffer::intersectedReset(KPropertySet&, KProperty&)
+KPropertyBuffer::intersect(KPropertySet const&)
+KPropertyFactoryManager::~KPropertyFactoryManager()
+KPropertyFactoryManager::KPropertyFactoryManager()
+KPropertyFactoryManager::addInitFunction(void (*)())
+KPropertyFactoryManager::canConvertValueToText(KProperty const*) const
+KPropertyFactoryManager::canConvertValueToText(int) const
+KPropertyFactoryManager::createComposedProperty(KProperty*)
+KPropertyFactoryManager::propertyValueToLocalizedString(KProperty const*) const
+KPropertyFactoryManager::propertyValueToString(KProperty const*) const
+KPropertyFactoryManager::registerFactory(KPropertyFactory*)
+KPropertyFactoryManager::self()
+KPropertyFactoryManager::valueToLocalizedString(int, QVariant const&) const
+KPropertyFactoryManager::valueToString(int, QVariant const&) const
+KPropertyFactory::~KPropertyFactory()
+KPropertyFactory::KPropertyFactory()
+KPropertyFactory::addComposedPropertyCreatorInternal(int, KComposedPropertyCreatorInterface*, bool)
+KPropertyFactory::addComposedPropertyCreator(int, KComposedPropertyCreatorInterface*)
+KPropertyFactory::addDisplayInternal(int, KPropertyValueDisplayInterface*, bool)
+KPropertyFactory::addDisplay(int, KPropertyValueDisplayInterface*)
+KPropertyFactory::composedPropertyCreators() const
+KPropertyFactory::valueDisplays() const
+KPropertySelector::~KPropertySelector()
+KPropertySelector::KPropertySelector()
+KPropertySetIterator::~KPropertySetIterator()
+KPropertySetIterator::KPropertySetIterator(KPropertySet const&)
+KPropertySetIterator::KPropertySetIterator(KPropertySet const&, KPropertySelector const&)
+KPropertySetIterator::operator++()
+KPropertySetIterator::setOrder(KPropertySetIterator::Order)
+KPropertySetIterator::skipNotAcceptable()
+KPropertySetPrivate::~KPropertySetPrivate()
+KPropertySetPrivate::KPropertySetPrivate(KPropertySet*)
+KPropertySetPrivate::addProperty(KProperty*, QByteArray const&)
+KPropertySetPrivate::addRelatedProperty(KProperty*, KProperty*) const
+KPropertySetPrivate::addToGroup(QByteArray const&, KProperty*)
+KPropertySetPrivate::clear()
+KPropertySetPrivate::copyAttributesFrom(KPropertySetPrivate const&)
+KPropertySetPrivate::copyPropertiesFrom(QList<KProperty*>::const_iterator const&, QList<KProperty*>::const_iterator const&, KPropertySet const&)
+KPropertySetPrivate::hasGroups() const
+KPropertySetPrivate::indexOfPropertyInGroup(KProperty const*) const
+KPropertySetPrivate::indexOfProperty(KProperty const*) const
+KPropertySetPrivate::informAboutClearing(bool*)
+KPropertySetPrivate::removeFromGroup(KProperty*)
+KPropertySetPrivate::removeProperty(KProperty*)
+KPropertySet::~KPropertySet()
+KPropertySet::KPropertySet(KPropertySet const&)
+KPropertySet::KPropertySet(QObject*)
+KPropertySet::KPropertySet(bool)
+KPropertySet::aboutToBeCleared()
+KPropertySet::aboutToBeDeleted()
+KPropertySet::aboutToDeleteProperty(KPropertySet&, KProperty&)
+KPropertySet::addProperty(KProperty*, QByteArray const&)
+KPropertySet::changePropertyIfExists(QByteArray const&, QVariant const&)
+KPropertySet::changeProperty(QByteArray const&, QVariant const&)
+KPropertySet::clear()
+KPropertySet::contains(QByteArray const&) const
+KPropertySet::count(KPropertySelector const&) const
+KPropertySet::count() const
+KPropertySet::debug() const
+KPropertySet::groupCaption(QByteArray const&) const
+KPropertySet::groupIconName(QByteArray const&) const
+KPropertySet::groupNames() const
+KPropertySet::hasProperties(KPropertySelector const&) const
+KPropertySet::hasVisibleProperties() const
+KPropertySet::isEmpty() const
+KPropertySet::isReadOnly() const
+KPropertySet::operator=(KPropertySet const&)
+KPropertySet::operator[](QByteArray const&) const
+KPropertySet::previousSelection() const
+KPropertySet::propertyChangedInternal(KPropertySet&, KProperty&)
+KPropertySet::propertyChanged(KPropertySet&, KProperty&)
+KPropertySet::propertyNamesForGroup(QByteArray const&) const
+KPropertySet::propertyReset(KPropertySet&, KProperty&)
+KPropertySet::propertyValues() const
+KPropertySet::propertyValue(QByteArray const&, QVariant const&) const
+KPropertySet::property(QByteArray const&) const
+KPropertySet::readOnlyFlagChanged()
+KPropertySet::removeProperty(KProperty*)
+KPropertySet::removeProperty(QByteArray const&)
+KPropertySet::setGroupCaption(QByteArray const&, QString const&)
+KPropertySet::setGroupIconName(QByteArray const&, QString const&)
+KPropertySet::setPreviousSelection(QByteArray const&)
+KPropertySet::setReadOnly(bool)
+KPropertyUnit::convertFromUnitToUnit(double, KPropertyUnit const&, KPropertyUnit const&, double)
+KPropertyUnit::fromListForUi(int, QFlags<KPropertyUnit::ListOption>, double)
+KPropertyUnit::fromSymbol(QString const&, bool*)
+KPropertyUnit::fromUserValue(QString const&, bool*) const
+KPropertyUnit::fromUserValue(double) const
+KPropertyUnit::indexInListForUi(QFlags<KPropertyUnit::ListOption>) const
+KPropertyUnit::listOfUnitNameForUi(QFlags<KPropertyUnit::ListOption>)
+KPropertyUnit::parseAngle(QString const&, double)
+KPropertyUnit::parseValue(QString const&, double)
+KPropertyUnit::ptToUnit(double, KPropertyUnit const&)
+KPropertyUnit::symbol() const
+KPropertyUnit::toUserStringValue(double) const
+KPropertyUnit::toUserValue(double) const
+KPropertyUnit::unitDescription(KPropertyUnit::Type)
+KPropertyUtils::keyForEnumValue(char const*, int)
+KPropertyValueDisplayInterface::~KPropertyValueDisplayInterface()
+KPropertyValueDisplayInterface::KPropertyValueDisplayInterface()
+KPropertyValueDisplayInterface::maxStringValueLength()
+KPropertyValueDisplayInterface::valueToLocalizedString(QVariant const&)
+*/
+
+QTEST_MAIN(KPropertyTest)
+
+#include "KPropertyTest.moc"
