ff32c6863c2e671a42c943f091bba65c2e2cf47a
Jaroslaw Staniek <staniek@kde.org>
2017-10-09 23:36:30 +0200

    Translations: split kproperty_qt into kpropertycore_qt & kpropertywidgets_qt
    Summary: https://phabricator.kde.org/T3992
    
    Test Plan: Run message extraction
    
    Reviewers: kossebau
    
    Tags: #kproperty
    
    Maniphest Tasks: T3992
    
    Differential Revision: https://phabricator.kde.org/D8230

--- src/Messages.sh
+++ src/Messages.sh
@@ -3,4 +3,28 @@
 # Extract strings from all source files.
 # EXTRACT_TR_STRINGS extracts strings with lupdate and convert them to .pot with
 # lconvert.
-$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml` -o $podir/kproperty_qt.pot
+
+function find_files()
+{
+    while IFS='' read -r line ; do
+        if echo "$line" | grep -q "${1}_LIB_SRCS" > /dev/null; then
+            while IFS=' ' read -a file ; do
+                if [ "$file" == ")" ]; then
+                    break
+                elif echo "$file" | grep -q "^\w*#" > /dev/null || [ ! -f "$file" ] || \
+                     echo "$file" | grep -qv "\.cpp\w*$" > /dev/null; then
+                    continue
+                fi
+                echo $file
+            done
+        fi
+    done < CMakeLists.txt
+}
+
+function extract()
+{
+    $EXTRACT_TR_STRINGS `find_files ${1}` -o $podir/${1}_qt.pot
+}
+
+extract kpropertycore
+extract kpropertywidgets
