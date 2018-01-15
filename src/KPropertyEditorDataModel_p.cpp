KDE/kproperty

commit be46e3f0768fd175e4eb8635ab5cf7c111d45f87

Add toolTipsVisible feature to KPropertyEditor, if enabled tooltips are visible for property editor items
Tooltips are displayed over each property item, both the name and
value column, and are equal to property descriptions (KProperty::description()).
Tooltips are not displayed for items having empty descriptions.

In addition:
- Do not crash when property set is not assigned and setGroupsVisible or setToolTipsVisible is called
- Rather store view settings (tooltips/groups settings) in view than in model
- Add more checks for presence of the internal model
- Add suitable examples to the KPropertyExample app

FIXED-IN:3.1.0

src/KPropertyEditorDataModel_p.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2008-2017 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2008-2018 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -20,27 +20,32 @@
 #include "KPropertyEditorDataModel_p.h"
 #include "KPropertyWidgetsFactory.h"
 #include "KPropertySet_p.h"
+#include "KPropertyEditorView.h"
 
 #include <QHash>
 
 class Q_DECL_HIDDEN KPropertyEditorDataModel::Private
 {
 public:
-    explicit Private(KPropertySet *_set, KPropertySetIterator::Order _order = KPropertySetIterator::Order::Insertion)
-        : set(_set), order(_order)
+    explicit Private(KPropertyEditorView *_view, KPropertySetIterator::Order _order = KPropertySetIterator::Order::Insertion)
+        : view(_view), order(_order)
     {
-        Q_ASSERT(set);
-        if (!set) {
+        Q_ASSERT(view);
+        if (!view) {
+            kprCritical() << "KPropertyEditorDataModel requires a KPropertyView object";
+        }
+        Q_ASSERT(view->propertySet());
+        if (!view->propertySet()) {
             kprCritical() << "KPropertyEditorDataModel requires a KPropertySet object";
         }
     }
-    inline KPropertySetPrivate* set_d() { return KPropertySetPrivate::d(set); }
-    KPropertySet *set;
+    inline KPropertySet *set() { return view->propertySet(); }
+    inline KPropertySetPrivate* set_d() { return KPropertySetPrivate::d(set()); }
+    KPropertyEditorView *view;
     KProperty rootItem;
     KProperty groupItem; //!< Pseudo group item used for all group items
     QHash<QByteArray, QPersistentModelIndex> indicesForNames;
     KPropertySetIterator::Order order; //!< order of properties
-    bool groupsVisible = true;
 };
 
 // -------------------
@@ -59,10 +64,10 @@ public:
 
 // -------------------
 
-KPropertyEditorDataModel::KPropertyEditorDataModel(KPropertySet *propertySet, QObject *parent,
+KPropertyEditorDataModel::KPropertyEditorDataModel(KPropertyEditorView *view,
                                                    KPropertySetIterator::Order order)
-        : QAbstractItemModel(parent)
-        , d(new Private(propertySet, order))
+        : QAbstractItemModel(view)
+        , d(new Private(view, order))
 {
     collectIndices();
 }
@@ -84,7 +89,7 @@ static inline bool nameAndCaptionLessThan(const NameAndCaption &n1, const NameAn
 void KPropertyEditorDataModel::collectIndices() const
 {
     d->indicesForNames.clear();
-    if (d->groupsVisible) {
+    if (d->view->groupsVisible()) {
         for (const QByteArray &groupName : d->set_d()->groupNames()) {
             const QList<QByteArray>* propertyNames = d->set_d()->propertyNamesForGroup(groupName);
             if (!propertyNames) {
@@ -98,7 +103,7 @@ void KPropertyEditorDataModel::collectIndices() const
             }
         }
     } else {
-        KPropertySetIterator it(*d->set, VisiblePropertySelector());
+        KPropertySetIterator it(*d->set(), VisiblePropertySelector());
         if (d->order == KPropertySetIterator::Order::Alphabetical) {
             it.setOrder(KPropertySetIterator::Order::Alphabetical);
         }
@@ -134,8 +139,16 @@ QVariant KPropertyEditorDataModel::data(const QModelIndex &index, int role) cons
 
     const int col = index.column();
     const KProperty *prop = propertyForIndex(index);
-    if (role == PropertyGroupRole) {
+    switch (role) {
+    case PropertyGroupRole:
         return prop == &d->groupItem;
+    case Qt::ToolTipRole:
+        if (d->view->toolTipsVisible() && !prop->description().isEmpty()) {
+            return prop->description();
+        }
+        break;
+    default:
+        break;
     }
     if (col == 0) {
         if (prop == &d->groupItem) {
@@ -143,9 +156,9 @@ QVariant KPropertyEditorDataModel::data(const QModelIndex &index, int role) cons
             Q_ASSERT(!groupName.isEmpty());
             switch(role) {
             case Qt::DisplayRole:
-                return d->set->groupCaption(groupName);
+                return d->set()->groupCaption(groupName);
             case Qt::DecorationRole:
-                return QIcon::fromTheme(d->set->groupIconName(groupName));
+                return QIcon::fromTheme(d->set()->groupIconName(groupName));
             default:;
             }
         } else if (role == Qt::DisplayRole) {
@@ -179,7 +192,7 @@ Qt::ItemFlags KPropertyEditorDataModel::flags(const QModelIndex &index) const
     const KProperty *prop = propertyForIndex(index);
     if (prop != &d->groupItem) {
         f |= Qt::ItemIsSelectable;
-        if (col == 1 && prop != &d->rootItem && !prop->isReadOnly() && !d->set->isReadOnly()) {
+        if (col == 1 && prop != &d->rootItem && !prop->isReadOnly() && !d->set()->isReadOnly()) {
             f |= Qt::ItemIsEditable;
         }
     }
@@ -217,12 +230,12 @@ QModelIndex KPropertyEditorDataModel::index(int row, int column, const QModelInd
 
     KProperty *parentItem = propertyForIndex(parent);
     KProperty *childItem = nullptr;
-    if (parentItem == &d->rootItem && d->groupsVisible && d->set_d()->hasGroups()) {
+    if (parentItem == &d->rootItem && d->view->groupsVisible() && d->set_d()->hasGroups()) {
         // top level with groups: return group item
         return createIndex(row, column, &d->groupItem);
     } else if (parentItem == &d->rootItem || parentItem == &d->groupItem) {
         // top level without groups or group level: return top-level visible property item
-        if (d->groupsVisible && d->set_d()->hasGroups()) {
+        if (d->view->groupsVisible() && d->set_d()->hasGroups()) {
             const QByteArray groupName(d->set_d()->groupName(parent.row()));
             const QList<QByteArray>* propertyNames = d->set_d()->propertyNamesForGroup(groupName);
             if (propertyNames) {
@@ -240,7 +253,7 @@ QModelIndex KPropertyEditorDataModel::index(int row, int column, const QModelInd
                 }
             }
         } else { // all properties, flat
-            KPropertySetIterator it(*d->set, VisiblePropertySelector());
+            KPropertySetIterator it(*d->set(), VisiblePropertySelector());
             if (d->order == KPropertySetIterator::Order::Alphabetical) {
                 it.setOrder(KPropertySetIterator::Order::Alphabetical);
             }
@@ -279,7 +292,7 @@ QModelIndex KPropertyEditorDataModel::parent(const QModelIndex &index) const
         const int indexOfParentItem = d->set_d()->indexOfProperty(parentItem);
         return createIndex(indexOfParentItem, 0, parentItem);
     }
-    if (d->groupsVisible && d->set_d()->hasGroups()) {
+    if (d->view->groupsVisible() && d->set_d()->hasGroups()) {
         // top-level property within a group: group item is the parent
         const QByteArray group(d->set_d()->groupForProperty(childItem));
         const int indexOfGroup = d->set_d()->indexOfGroup(group);
@@ -292,10 +305,10 @@ int KPropertyEditorDataModel::rowCount(const QModelIndex &parent) const
 {
     KProperty *parentItem = propertyForIndex(parent);
     if (parentItem == &d->rootItem) { // top level: return group count or top-level properties count
-        if (d->groupsVisible && d->set_d()->hasGroups()) {
+        if (d->view->groupsVisible() && d->set_d()->hasGroups()) {
             return d->set_d()->groupNames().count();
         }
-        return d->set->count(VisiblePropertySelector()); // number of visible properties
+        return d->set()->count(VisiblePropertySelector()); // number of visible properties
     } else if (parentItem == &d->groupItem) { // group level: return property count within the group
         const QByteArray groupName = d->set_d()->groupName(parent.row());
         Q_ASSERT(!groupName.isEmpty());
@@ -348,7 +361,7 @@ QModelIndex KPropertyEditorDataModel::buddy(const QModelIndex & idx) const
 
 KPropertySet* KPropertyEditorDataModel::propertySet() const
 {
-    return d->set;
+    return d->set();
 }
 
 void KPropertyEditorDataModel::setOrder(KPropertySetIterator::Order order)
@@ -368,7 +381,7 @@ bool KPropertyEditorDataModel::hasChildren(const QModelIndex & parent) const
 {
     KProperty *parentItem = propertyForIndex(parent);
     if (parentItem == &d->rootItem) { // top level
-        return d->set->hasVisibleProperties();
+        return d->set()->hasVisibleProperties();
     } else if (parentItem == &d->groupItem) { // group level
         const QByteArray groupName(d->set_d()->groupName(parent.row()));
         Q_ASSERT(!groupName.isEmpty());
@@ -386,18 +399,9 @@ bool KPropertyEditorDataModel::hasChildren(const QModelIndex & parent) const
     return children && !children->isEmpty();
 }
 
-bool KPropertyEditorDataModel::groupsVisible() const
+void KPropertyEditorDataModel::updateGroupsVisibility()
 {
-    return d->groupsVisible;
-}
-
-void KPropertyEditorDataModel::setGroupsVisible(bool set)
-{
-    if (d->groupsVisible == set) {
-        return;
-    }
     beginResetModel();
-    d->groupsVisible = set;
     collectIndices();
     endResetModel();
 }

examples/window.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 2004 Cedric Pasteur <cedric.pasteur@free.fr>
-   Copyright (C) 2008-2017 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2008-2018 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -70,6 +70,8 @@ Window::Window()
     }
 
     /*  First, create a KPropertySet which will hold the properties.  */
+    connect(&m_set, &KPropertySet::propertyChanged, this, &Window::propertyChanged);
+
     KProperty *p = nullptr;
     m_set.setReadOnly(m_parser.isSet(m_roOption));
     QByteArray group;
@@ -312,8 +314,19 @@ Window::Window()
         p->setReadOnly(true);
     }
 
+    // Properties for testing tooltips
+    if (singleProperty.isEmpty() || singleProperty == "StaticToolTip") {
+        m_set.addProperty(new KProperty("StaticToolTip", "Some value", "Static Tooltip",
+          "This is a static tooltip based on value of KProperty::description()"), group);
+        m_set.addProperty(
+            m_dynamicToolTipProperty = new KProperty("DynamicToolTip", "Some dynamic value",
+                                                     "Dynamic Tooltip"), group);
+        propertyChanged(m_set, *m_dynamicToolTipProperty); // this updates description
+    }
+
     QVBoxLayout *lyr = new QVBoxLayout(this);
     m_editorView = new KPropertyEditorView(this);
+    m_editorView->setToolTipsVisible(true);
     lyr->addWidget(m_editorView);
     m_editorView->changeSet(&m_set);
     m_editorView->resizeColumnToContents(0);
@@ -340,6 +353,11 @@ Window::Window()
         showGroups->setEnabled(false);
     }
     hlyr->addWidget(showGroups);
+
+    QCheckBox *showToolTips = new QCheckBox("Show tooltips");
+    connect(showToolTips, &QCheckBox::toggled, m_editorView, &KPropertyEditorView::setToolTipsVisible);
+    showToolTips->setChecked(m_editorView->toolTipsVisible());
+    hlyr->addWidget(showToolTips);
 
     QCheckBox *readOnly = new QCheckBox("Read-only");
     connect(readOnly, &QCheckBox::toggled, &m_set, &KPropertySet::setReadOnly);
@@ -374,4 +392,14 @@ void Window::showGrid(int state)
 void Window::showFrame(int state)
 {
     m_editorView->setFrameStyle(state == Qt::Checked ? QFrame::Box : QFrame::NoFrame);
+}
+
+void Window::propertyChanged(KPropertySet& set, KProperty& property)
+{
+    Q_UNUSED(set)
+    if (&property == m_dynamicToolTipProperty) {
+        property.setDescription(QString::fromLatin1("This is a dynamic tooltip based on value of "
+                                                    "\"%1\" property which is currently \"%2\"")
+                                    .arg(property.caption(), property.value().toString()));
+    }
 }

examples/window.h
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
    Copyright (C) 2004-2005 Cedric Pasteur <cedric.pasteur@free.fr>
-   Copyright (C) 2008-2017 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2008-2018 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -40,6 +40,9 @@ protected Q_SLOTS:
     void showGrid(int state);
     void showFrame(int state);
 
+    //! Reaction on changed value of property
+    void propertyChanged(KPropertySet& set, KProperty& property);
+
 private:
     void parseCommandLine();
 
@@ -50,6 +53,7 @@ private:
     QCommandLineOption m_propertyOption;
     QCommandLineOption m_roOption;
     KPropertyEditorView *m_editorView;
+    KProperty *m_dynamicToolTipProperty;
 };
 
 #endif

src/KPropertyEditorDataModel_p.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2008-2017 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2008-2018 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -27,6 +27,7 @@
 #include <QModelIndex>
 
 class KProperty;
+class KPropertyEditorView;
 
 /*! @short A data model that integrates a KPropertySet object with the Qt's model/view API.
  @see KPropertyEditorView
@@ -36,8 +37,8 @@ class KPropertyEditorDataModel : public QAbstractItemModel
 {
     Q_OBJECT
 public:
-    //! Creates a new model. @a propertySet is required.
-    explicit KPropertyEditorDataModel(KPropertySet *propertySet, QObject *parent = nullptr,
+    //! Creates a new model. @a view and view->propertySet() are required.
+    explicit KPropertyEditorDataModel(KPropertyEditorView *view,
                                       KPropertySetIterator::Order order = KPropertySetIterator::Order::Insertion);
     ~KPropertyEditorDataModel() override;
 
@@ -84,19 +85,14 @@ public:
     //! Reimplemented for optimization.
     bool hasChildren(const QModelIndex &parent = QModelIndex()) const override;
 
-    /*! @return @c true if the property groups should be visible.
-     @see KPropertyEditorView::groupsVisible()
-     @since 3.1 */
-    bool groupsVisible() const;
-
 public Q_SLOTS:
     //! Sets order for properties.
     void setOrder(KPropertySetIterator::Order order);
 
-    /*! Shows the property groups if @a set is @c true.
+    /*! Updates visibility of property groups.
      @see KPropertyEditorView::setGroupsVisible(bool)
      @since 3.1 */
-    void setGroupsVisible(bool set);
+    void updateGroupsVisibility();
 
 private:
     //! Collects persistent indices for the model. They are dependent on groupping and sorting.

src/KPropertyEditorView.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2008-2017 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2008-2018 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -256,8 +256,7 @@ class Q_DECL_HIDDEN KPropertyEditorView::Private
 {
 public:
     explicit Private(KPropertyEditorView *view)
-     : set(nullptr)
-     , model(nullptr)
+     : model(nullptr)
      , gridLineColor( KPropertyEditorView::defaultGridLineColor() )
      , valueSync(true)
      , slotPropertyChangedEnabled(true)
@@ -265,8 +264,11 @@ public:
     {
     }
 
-    //! Expands group and parent propertiey items if needed (based on settings)
+    //! Expands group and parent property items if needed (based on settings)
     void expandIfNeeded() {
+        if (!model) {
+            return;
+        }
         const int rowCount = model->rowCount();
         for (int row = 0; row < rowCount; row++) {
             expandChildItemsIfNeeded(model->index(row, 0));
@@ -275,6 +277,9 @@ public:
 
     //! Expands property child items in a subtree recursively if needed (based on settings)
     void expandChildItemsIfNeeded(const QModelIndex &parent) {
+        if (!model) {
+            return;
+        }
         const bool isGroupHeader(model->data(parent, KPropertyEditorDataModel::PropertyGroupRole).toBool());
         if (isGroupHeader) {
             if (groupItemsExpanded) {
@@ -292,7 +297,7 @@ public:
         }
     }
 
-    KPropertySet *set;
+    QPointer<KPropertySet> set;
     KPropertyEditorDataModel *model;
     ItemDelegate *itemDelegate;
     QColor gridLineColor;
@@ -300,6 +305,8 @@ public:
     bool slotPropertyChangedEnabled;
     bool childPropertyItemsExpanded = true;
     bool groupItemsExpanded = true;
+    bool groupsVisible = true;
+    bool toolTipsVisible = false;
 
 private:
     KPropertyEditorView * const q;
@@ -426,7 +433,7 @@ void KPropertyEditorView::changeSetInternal(KPropertySet *set, SetOptions option
         = (options & SetOption::AlphabeticalOrder)
             ? KPropertySetIterator::Order::Alphabetical
             : KPropertySetIterator::Order::Insertion;
-    d->model = d->set ? new KPropertyEditorDataModel(d->set, this, setOrder) : nullptr;
+    d->model = d->set ? new KPropertyEditorDataModel(this, setOrder) : nullptr;
     setModel( d->model );
     delete oldModel;
 
@@ -503,16 +510,18 @@ bool KPropertyEditorView::groupItemsExpanded() const
 
 bool KPropertyEditorView::groupsVisible() const
 {
-    return d->model->groupsVisible();
+    return d->groupsVisible;
 }
 
 void KPropertyEditorView::setGroupsVisible(bool set)
 {
-    if (d->model->groupsVisible() == set) {
+    if (d->groupsVisible == set) {
         return;
     }
-    d->model->setGroupsVisible(set);
-    d->expandIfNeeded();
+    if (d->model) {
+        d->model->updateGroupsVisibility();
+        d->expandIfNeeded();
+    }
     viewport()->update();
 }
 
@@ -546,6 +555,9 @@ void KPropertyEditorView::drawBranches( QPainter * painter, const QRect & rect, 
 
 void KPropertyEditorView::drawRow(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
+    if (!d->model) {
+        return;
+    }
     const KPropertyUtilsPrivate::PainterSaver saver(painter);
     const bool isGroupHeader(d->model->data(index, KPropertyEditorDataModel::PropertyGroupRole).toBool());
     QStyleOptionViewItem alteredOption(option);
@@ -566,7 +578,7 @@ void KPropertyEditorView::drawRow(QPainter *painter, const QStyleOptionViewItem 
 
 QRect KPropertyEditorView::revertButtonArea( const QModelIndex& index ) const
 {
-    if (index.column() != 0)
+    if (index.column() != 0 || !d->model)
         return QRect();
     QVariant modifiedVariant( d->model->data(index, KPropertyEditorDataModel::PropertyModifiedRole) );
     if (!modifiedVariant.isValid() || !modifiedVariant.toBool())
@@ -600,7 +612,7 @@ void KPropertyEditorView::mousePressEvent ( QMouseEvent * event )
 
 void KPropertyEditorView::undo()
 {
-    if (!d->set || d->set->isReadOnly())
+    if (!d->set || d->set->isReadOnly() || !d->model)
         return;
 
     KProperty *property = d->model->propertyForIndex(currentIndex());
@@ -642,7 +654,7 @@ QSize KPropertyEditorView::sizeHint() const
 
 KPropertySet* KPropertyEditorView::propertySet() const
 {
-    return d->model ? d->model->propertySet() : nullptr;
+    return d->set;
 }
 
 QColor KPropertyEditorView::gridLineColor() const
@@ -680,7 +692,7 @@ static QModelIndex findChildItem(const KProperty& property, const QModelIndex &p
 void KPropertyEditorView::slotPropertyChanged(KPropertySet& set, KProperty& property)
 {
     Q_UNUSED(set);
-    if (!d->slotPropertyChangedEnabled)
+    if (!d->slotPropertyChangedEnabled || !d->model)
         return;
     d->slotPropertyChangedEnabled = false;
     KProperty *realProperty = &property;
@@ -697,7 +709,7 @@ void KPropertyEditorView::slotPropertyChanged(KPropertySet& set, KProperty& prop
 
 void KPropertyEditorView::updateSubtree(const QModelIndex &index)
 {
-    if (!index.isValid()) {
+    if (!index.isValid() || !d->model) {
         return;
     }
     update(index);
@@ -719,4 +731,15 @@ void KPropertyEditorView::slotPropertyReset(KPropertySet& set, KProperty& proper
 {
 //! @todo OK?
     slotPropertyChanged(set, property);
+}
+
+
+bool KPropertyEditorView::toolTipsVisible() const
+{
+    return d->toolTipsVisible;
+}
+
+void KPropertyEditorView::setToolTipsVisible(bool set)
+{
+    d->toolTipsVisible = set;
 }

src/KPropertyEditorView.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2008-2017 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2008-2018 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -88,6 +88,18 @@ public:
      @since 3.1 */
     bool groupItemsExpanded() const;
 
+    /**
+     * Returns @c true if the property editor widget has enabled visibility of tooltips
+     *
+     * Tooltips are displayed over each property item, both the name and value column, and are
+     * equal to property descriptions (KProperty::description()). Tooltips are not displayed for
+     * items having empty descriptions.
+     *
+     * Tooltips visibility is disabled by default.
+     * @since 3.1
+     */
+    bool toolTipsVisible() const;
+
 public Q_SLOTS:
     /*! Populates the editor view with items for each property from the @a set set.
      Child items for composed properties are also created.
@@ -145,6 +157,14 @@ public Q_SLOTS:
      @note To expand all items use expandAll(). To collapse all items use collapseAll().
      @since 3.1 */
     void setGroupItemsExpanded(bool set);
+
+    /**
+     * If @a set is @c true tooltips are visible for property editor items
+     *
+     * See toolTipsVisible() for details.
+     * @since 3.1
+     */
+    void setToolTipsVisible(bool set);
 
 Q_SIGNALS:
     /*! Emitted when current property set has been changed. May be 0. */

commit 111d5b8f4e240e9383d19e91e9b514806a0ef144

GIT_SILENT Fix possible crashes introduced in be46e3f0768

src/KPropertyEditorDataModel_p.cpp
@@ -89,6 +89,9 @@ static inline bool nameAndCaptionLessThan(const NameAndCaption &n1, const NameAn
 void KPropertyEditorDataModel::collectIndices() const
 {
     d->indicesForNames.clear();
+    if (!d->set()) {
+        return;
+    }
     if (d->view->groupsVisible()) {
         for (const QByteArray &groupName : d->set_d()->groupNames()) {
             const QList<QByteArray>* propertyNames = d->set_d()->propertyNamesForGroup(groupName);
@@ -379,6 +382,9 @@ KPropertySetIterator::Order KPropertyEditorDataModel::order() const
 
 bool KPropertyEditorDataModel::hasChildren(const QModelIndex & parent) const
 {
+    if (!d->set()) {
+        return false;
+    }
     KProperty *parentItem = propertyForIndex(parent);
     if (parentItem == &d->rootItem) { // top level
         return d->set()->hasVisibleProperties();